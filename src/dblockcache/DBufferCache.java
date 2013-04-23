43e0919eb79c6e0f49f927e3e7682a3af220f892
Chris Graybeal <git@github.com:ckgraybeal>
2013-04-22 22:21:13 -0400

    chris

--- src/dblockcache/DBufferCache.java
+++ src/dblockcache/DBufferCache.java
@@ -10,75 +10,76 @@ import common.Constants;
 
 public class DBufferCache {
 
-    private int _cacheSize;
-    private VirtualDisk myDisk;
-    private Queue<DBuffer> myDBuffers;
+	private int _cacheSize;
+	private VirtualDisk myDisk;
+	private Queue<DBuffer> myDBuffers;
 
-    /*
-     * Constructor: allocates a cacheSize number of cache blocks, each
-     * containing BLOCK-size bytes data, in memory
-     */
-    public DBufferCache(int cacheSize) {
-	_cacheSize = cacheSize * Constants.BLOCK_SIZE;
+	/*
+	 * Constructor: allocates a cacheSize number of cache blocks, each
+	 * containing BLOCK-size bytes data, in memory
+	 */
+	public DBufferCache(int cacheSize) {
+		_cacheSize = cacheSize * Constants.BLOCK_SIZE;
 
-	myDisk = VirtualDisk.getInstance();
+		myDisk = VirtualDisk.getInstance();
 
-	myDBuffers = new LinkedList<DBuffer>();// create a list of MyDBuffers of
-					       // size cacheSize
-	for (int i = 0; i < cacheSize; i++) {
-	    myDBuffers.add(new DBuffer());
+		myDBuffers = new LinkedList<DBuffer>();// create a list of MyDBuffers of
+												// size cacheSize
+		for (int i = 0; i < cacheSize; i++) {
+			myDBuffers.add(new DBuffer());
+		}
 	}
-    }
 
-    /*
-     * Get buffer for block specified by blockID. The buffer is "held" until the
-     * caller releases it. A "held" buffer cannot be evicted: its block ID
-     * cannot change.
-     */
-    public DBuffer getBlock(int blockID) {// returns a DBuffer from cache if it
-					  // exists or returns the least
-					  // recently used one
-	DBuffer DB = blockInCache(blockID);
-	if (DB == null) {
-	    DB = myDBuffers.poll();
+	/*
+	 * Get buffer for block specified by blockID. The buffer is "held" until the
+	 * caller releases it. A "held" buffer cannot be evicted: its block ID
+	 * cannot change.
+	 */
+	public DBuffer getBlock(int blockID) {// returns a DBuffer from cache if it
+											// exists or returns the least
+											// recently used one
+		DBuffer DB = blockInCache(blockID);
+		if (DB == null) {
+			DB = myDBuffers.poll();
+		}
+		DB.setBlockID(blockID);
+		myDBuffers.add(DB);
+		DB.holdBuffer();
+		return DB;
+
 	}
-	myDBuffers.add(DB);
-	DB.holdBuffer();
-	return DB;
 
-    }
-
-    public DBuffer blockInCache(int blockID)// searches for DBuffer that
-					    // represents block is "blockID" in
-					    // cache. Null if it is not
-    {
-	LinkedList<DBuffer> DBuffers = (LinkedList) myDBuffers;
-	for (int i = 0; i < myDBuffers.size(); i++) {
-	    DBuffer DB = DBuffers.get(i);
-	    if (DB.getBlockID() == blockID) {
-		return DBuffers.remove(i);
-	    }
+	public DBuffer blockInCache(int blockID)// searches for DBuffer that
+											// represents block is "blockID" in
+											// cache. Null if it is not
+	{
+		LinkedList<DBuffer> DBuffers = (LinkedList) myDBuffers;
+		for (int i = 0; i < myDBuffers.size(); i++) {
+			DBuffer DB = DBuffers.get(i);
+			if (DB.getBlockID() == blockID) {
+				return DBuffers.remove(i);
+			}
+		}
+		return null;
 	}
-	return null;
-    }
 
-    /* Release the buffer so that others waiting on it can use it */
-    public void releaseBlock(DBuffer buf) {
-	buf.releaseBuffer();
-    }
-
-    /*
-     * sync() writes back all dirty blocks to the volume and wait for
-     * completion. The sync() method should maintain clean block copies in
-     * DBufferCache.
-     */
-    public void sync() {
-	LinkedList<DBuffer> DBuffers = (LinkedList) myDBuffers;
-	for (int i = 0; i < DBuffers.size(); i++) {
-	    DBuffer db = DBuffers.get(i);
-	    if (!db.checkClean()) {
-		db.startPush();
-	    }
+	/* Release the buffer so that others waiting on it can use it */
+	public void releaseBlock(DBuffer buf) {
+		buf.releaseBuffer();
 	}
-    }
+
+	/*
+	 * sync() writes back all dirty blocks to the volume and wait for
+	 * completion. The sync() method should maintain clean block copies in
+	 * DBufferCache.
+	 */
+	public void sync() {
+		LinkedList<DBuffer> DBuffers = (LinkedList) myDBuffers;
+		for (int i = 0; i < DBuffers.size(); i++) {
+			DBuffer db = DBuffers.get(i);
+			if (!db.checkClean()) {
+				db.startPush();
+			}
+		}
+	}
 }
--- src/dfs/DFS.java
+++ src/dfs/DFS.java
@@ -19,186 +19,214 @@ import dblockcache.DBufferCache;
 
 public class DFS {
 
-    private boolean _format;
-    private String _volName;
-    private int numINodeBlocks;
-    // private ArrayList<INode> iNodes;
-    // private int[] DFileIds;//index represents ID, 1 represents used, 0
-    // represents free
-    private Map<DFileID, INode> myINodes; // Integer will be a DFID, all INODES
-					  // will be mapped to a specific DFID
-					  // whether or not they are in use
+	private boolean _format;
+	private String _volName;
+	private int numINodeBlocks;
+//	private ArrayList<INode> iNodes;
+//	private int[] DFileIds;//index represents ID, 1 represents used, 0 represents free 
+	private Map<DFileID, INode> myINodes; //Integer will be a DFID, all INODES will be mapped to a specific DFID whether or not they are in use
     private int[] availableBlocks;
-    private static DFS instance = null;
-    private DBufferCache myBufferCache;
+	private static DFS instance = null;
+	private DBufferCache myBufferCache;
 
-    /*
-     * @volName: Explicitly overwrite volume name
-     * 
-     * @format: If format is true, the system should earse the underlying disk
-     * contents and reinialize the volume.
-     */
+	/*
+	 * @volName: Explicitly overwrite volume name
+	 * 
+	 * @format: If format is true, the system should earse the underlying disk
+	 * contents and reinialize the volume.
+	 */
 
-    private DFS(String volName, boolean format) {
-	_volName = volName;
-	_format = format;
-	numINodeBlocks = 26214;
-	myBufferCache = new DBufferCache(Constants.NUM_OF_CACHE_BLOCKS);
-	// DFileIds = new int[numINodes];
-	// for(int i = 0; i< numINodes; i++)
-	// {
-	// DFileIds[i] = 0;
-	// }
-	// iNodes = new ArrayList<INode>();
-	availableBlocks = new int[Constants.NUM_OF_BLOCKS
-		- (numINodeBlocks + 1)];// this should be the number of blocks
-					// int he data region
-	myINodes = new HashMap<DFileID, INode>();
-	init();// reads all inodes form disk into iNodes map
-    }
-
-    private DFS(boolean format) {
-	this(Constants.vdiskName, format);
-    }
-
-    private DFS() {
-	this(Constants.vdiskName, false);
-    }
-
-    public static DFS getInstance() {
-	if (instance == null) {
-	    return new DFS();
+	private DFS(String volName, boolean format) {
+		_volName = volName;
+		_format = format;
+		numINodeBlocks = 26214;
+		myBufferCache = new DBufferCache(Constants.NUM_OF_CACHE_BLOCKS);
+//		DFileIds = new int[numINodes];
+//		for(int i = 0; i< numINodes; i++)
+//		{
+//			DFileIds[i] = 0;
+//		}
+//		iNodes = new ArrayList<INode>();
+		availableBlocks = new int[Constants.NUM_OF_BLOCKS-(numINodeBlocks+1)];//this should be the number of blocks int he data region
+		myINodes = new HashMap<DFileID, INode>();
+		init();//reads all inodes form disk into iNodes map
 	}
-	return instance;
-    }
 
-    /*
-     * Initialize all the necessary structures with sizes as specified in the
-     * common/Constants.java
-     */
-    public void init() {
-	try {
-	    File disk = new File(_volName, "rws");
-	    FileInputStream in = new FileInputStream(disk);
-	    byte[] blockRead = new byte[1024];
-	    in.read(blockRead);// block 0 is empty/ has metadata
+	private DFS(boolean format) {
+		this(Constants.vdiskName, format);
+	}
 
-	    // reading in iNodes
-	    for (int i = 0; i < numINodeBlocks; i++) {
-		in.read(blockRead);// read in first block with 32 iNodes
+	private DFS() {
+		this(Constants.vdiskName, false);
+	}
 
-		for (int j = 0; j < 32; j++) {
-		    byte[] node = Arrays.copyOfRange(blockRead, j * 32,
-			    (j + 1) * 32);
-		    int fileID = byteArrayToInt(node, 0);
-		    int[] blockMap = new int[7];
-		    if (fileID > 0)// if this inode holds a reference to a DFID
-		    {
+	public static DFS getInstance() {
+		if (instance == null) {
+			return new DFS();
+		}
+		return instance;
+	}
 
-			for (int k = 0; k < 7; k++) {
-			    int blockLocation = byteArrayToInt(node,
-				    4 + (k * 4));
-			    if (blockLocation >= 0) {
-				availableBlocks[blockLocation] = 1;
-			    }
-			    blockMap[k] = blockLocation;
+	/*
+	 * Initialize all the necessary structures with sizes as specified in the
+	 * common/Constants.java
+	 */
+	public void init() {
+		try {
+			File disk = new File(_volName, "rws");
+			FileInputStream in = new FileInputStream(disk);
+			byte[] blockRead = new byte[1024];
+			in.read(blockRead);// block 0 is empty/ has metadata
+
+			// reading in iNodes
+			for (int i = 0; i < numINodeBlocks; i++) {
+				in.read(blockRead);// read in first block with 32 iNodes
+
+				for (int j = 0; j < 32; j++) {
+					byte[] node = Arrays.copyOfRange(blockRead, j * 32,
+							(j + 1) * 32);
+					int fileID = byteArrayToInt(node, 0);
+					int[] blockMap = new int[7];
+					if (fileID > 0)// if this inode holds a reference to a DFID
+					{
+
+						for (int k = 0; k < 7; k++) {
+							int blockLocation = byteArrayToInt(node, 4 + (k * 4));
+							if(blockLocation >= 0)
+							{
+								availableBlocks[blockLocation] = 1;
+							}
+							blockMap[k] = blockLocation;
+						}
+					}
+					DFileID DFID = new DFileID(fileID);
+					INode currentNode = new INode(DFID, blockMap);
+//					iNodes.add(currentNode);
+//					DFileIds[fileID] = 1;
+					myINodes.put(DFID, currentNode);
+				}
+
 			}
-		    }
-		    DFileID DFID = new DFileID(fileID);
-		    INode currentNode = new INode(DFID, blockMap);
-		    // iNodes.add(currentNode);
-		    // DFileIds[fileID] = 1;
-		    myINodes.put(DFID, currentNode);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-	    }
-	} catch (FileNotFoundException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
 	}
 
-    }
-
-    public int byteArrayToInt(byte[] b, int startingIndex) {
-	int value = 0;
-	for (int i = 0; i < 4; i++) {
-	    int shift = (4 - 1 - i) * 8;
-	    value += (b[startingIndex + i] & 0x000000FF) << shift;
+	public int byteArrayToInt(byte[] b, int startingIndex) {
+		int value = 0;
+		for (int i = 0; i < 4; i++) {
+			int shift = (4 - 1 - i) * 8;
+			value += (b[startingIndex + i] & 0x000000FF) << shift;
+		}
+		return value;
 	}
-	return value;
-    }
 
-    /*
-     * creates a new DFile and returns the DFileID, which is useful to uniquely
-     * identify the DFile
-     */
-    public DFileID createDFile() {
-	INode available = getFreeINode();
-	if (available != null) {
-	    return available.getId();
+	/*
+	 * creates a new DFile and returns the DFileID, which is useful to uniquely
+	 * identify the DFile
+	 */
+	public DFileID createDFile() {
+		INode available  =  getFreeINode();
+		if(available!= null)
+		{
+			return available.getId();
+		}
+		return null;
 	}
-	return null;
-    }
 
-    /* destroys the file specified by the DFileID */
-    public void destroyDFile(DFileID dFID) {// i have no idea what im doing
-	INode toDestroy = myINodes.get(dFID);
-	int[] bMap = toDestroy.getBlockMap();
-	for (int i = 0; i < bMap.length; i++) {
-	    availableBlocks[bMap[i]] = 0;
+	/* destroys the file specified by the DFileID */
+	public void destroyDFile(DFileID dFID) {//i have no idea what im doing
+		INode toDestroy = myINodes.get(dFID);
+		int[] bMap = toDestroy.getBlockMap();
+		for(int i = 0; i< bMap.length; i++)
+		{
+			availableBlocks[bMap[i]] = 0;
+		}
+		toDestroy.setId(-1);//tommy can change this later
 	}
-	toDestroy.setId(-1);
-    }
 
-    /*
-     * reads the file dfile named by DFileID into the buffer starting from the
-     * buffer offset startOffset; at most count bytes are transferred
-     */
-    public int read(DFileID dFID, byte[] buffer, int startOffset, int count) {
+	/*
+	 * reads the file dfile named by DFileID into the buffer starting from the
+	 * buffer offset startOffset; at most count bytes are transferred
+	 */
+	public int read(DFileID dFID, byte[] buffer, int startOffset, int count) {
 
-	DBuffer db = myBufferCache.getBlock(dFID.getDFileID());
-	db.setBlockID(dFID.getDFileID());
-	db.read(buffer, startOffset, count);
-	return 0;
-    }
-
-    /*
-     * writes to the file specified by DFileID from the buffer starting from the
-     * buffer offset startOffset; at most count bytes are transferred
-     */
-    public int write(DFileID dFID, byte[] buffer, int startOffset, int count) {
-	return 0;
-    }
-
-    /* returns the size in bytes of the file indicated by DFileID. */
-    public int sizeDFile(DFileID dFID) {
-	return 0;
-    }
-
-    /*
-     * List all the existing DFileIDs in the volume
-     */
-    public List<DFileID> listAllDFiles() {
-	return null;
-    }
-
-    /* Write back all dirty blocks to the volume, and wait for completion. */
-    public void sync() {
-	myBufferCache.sync();
-	// TODO also have to sync back all of the inodes
-    }
-
-    public INode getFreeINode() {
-	for (int i = 0; i < numINodeBlocks * 32; i++) {
-	    INode node = myINodes.get(i);
-	    if (node.getId().getDFileID() <= 0) {
-		node.setId(i);
-		return node;
-	    }
+		INode fileNode = myINodes.get(dFID);
+		int[] blockMap = fileNode.getBlockMap();
+		for(int i = 0; i < blockMap.length; i++)
+		{
+			
+			if(blockMap[i]>0 && count>=0)
+			{
+				DBuffer db = myBufferCache.getBlock(blockMap[i]);
+				
+				if(count>Constants.BLOCK_SIZE)
+				{
+					db.read(buffer, startOffset+1024*i, Constants.BLOCK_SIZE);
+					count-=Constants.BLOCK_SIZE;
+				}
+				else{
+					db.read(buffer, startOffset+1024*i, count);
+					count-=count;
+				}
+				
+				
+				
+			}
+			
+		}
+		
+		int blockNum = startOffset/Constants.BLOCK_SIZE;
+		
+		
+		
+		
+		
+		return 0;
 	}
-	return null;
-    }
+
+	/*
+	 * writes to the file specified by DFileID from the buffer starting from the
+	 * buffer offset startOffset; at most count bytes are transferred
+	 */
+	public int write(DFileID dFID, byte[] buffer, int startOffset, int count) {
+		return 0;
+	}
+
+	/* returns the size in bytes of the file indicated by DFileID. */
+	public int sizeDFile(DFileID dFID) {
+		return 0;
+	}
+
+	/*
+	 * List all the existing DFileIDs in the volume
+	 */
+	public List<DFileID> listAllDFiles() {
+		return null;
+	}
+
+	/* Write back all dirty blocks to the volume, and wait for completion. */
+	public void sync() {
+		myBufferCache.sync();
+		//TODO also have to sync back all of the inodes
+	}
+	
+	public INode getFreeINode()
+	{
+		for(int i = 0; i< numINodeBlocks*32; i++)
+		{
+			INode node = myINodes.get(i);
+			if(node.getId().getDFileID()<=0)
+			{
+				node.setId(i);
+				return node;
+			}
+		}
+		return null;
+	}
 }
